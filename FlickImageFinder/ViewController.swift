57053e480000b9d278a571ba40da70417502baa5
David Ilenwabor <dave11ilen@gmail.com>
2018-06-09 02:15:05 +0100

    Search functionality added

--- FlickImageFinder/ViewController.swift
+++ FlickImageFinder/ViewController.swift
@@ -10,16 +10,285 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var keyboardOnScreen = false
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var photoTitleLabel: UILabel!
+    @IBOutlet weak var phraseTextField: UITextField!
+    @IBOutlet weak var phraseSearchButton: UIButton!
+    @IBOutlet weak var latitudeTextField: UITextField!
+    @IBOutlet weak var longitudeTextField: UITextField!
+    @IBOutlet weak var latLonSearchButton: UIButton!
+    
+    // MARK: Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        phraseTextField.delegate = self
+        latitudeTextField.delegate = self
+        longitudeTextField.delegate = self
+        subscribeToNotification(.UIKeyboardWillShow, selector: #selector(keyboardWillShow))
+        subscribeToNotification(.UIKeyboardWillHide, selector: #selector(keyboardWillHide))
+        subscribeToNotification(.UIKeyboardDidShow, selector: #selector(keyboardDidShow))
+        subscribeToNotification(.UIKeyboardDidHide, selector: #selector(keyboardDidHide))
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unsubscribeFromAllNotifications()
     }
-
-
+    
+    // MARK: Search Actions
+    
+    @IBAction func searchByPhrase(_ sender: AnyObject) {
+        
+        userDidTapView(self)
+        setUIEnabled(false)
+        
+        if !phraseTextField.text!.isEmpty {
+            photoTitleLabel.text = "Searching..."
+            let methodParameters: [String: Any] = [Constants.FlickrParameterKeys.Method: Constants.FlickrParameterValues.SearchMethod, Constants.FlickrParameterKeys.APIKey : Constants.FlickrParameterValues.APIKey, Constants.FlickrParameterKeys.Text: phraseTextField.text!, Constants.FlickrParameterKeys.SafeSearch: Constants.FlickrParameterValues.UseSafeSearch, Constants.FlickrParameterKeys.Extras: Constants.FlickrParameterValues.MediumURL, Constants.FlickrParameterKeys.Format: Constants.FlickrParameterValues.ResponseFormat, Constants.FlickrParameterKeys.NoJSONCallback: Constants.FlickrParameterValues.DisableJSONCallback
+            ]
+            displayImageFromFlickrBySearch(methodParameters)
+        } else {
+            setUIEnabled(true)
+            photoTitleLabel.text = "Phrase Empty."
+        }
+    }
+    
+    @IBAction func searchByLatLon(_ sender: AnyObject) {
+        
+        userDidTapView(self)
+        setUIEnabled(false)
+        
+        if isTextFieldValid(latitudeTextField, forRange: Constants.Flickr.SearchLatRange) && isTextFieldValid(longitudeTextField, forRange: Constants.Flickr.SearchLonRange) {
+            photoTitleLabel.text = "Searching..."
+            // TODO: Set necessary parameters!
+            let methodParameters: [String: Any] = [Constants.FlickrParameterKeys.Method: Constants.FlickrParameterValues.SearchMethod, Constants.FlickrParameterKeys.APIKey : Constants.FlickrParameterValues.APIKey,
+                Constants.FlickrParameterKeys.BoundingBox: bboxString(),
+                Constants.FlickrParameterKeys.SafeSearch: Constants.FlickrParameterValues.UseSafeSearch,
+                Constants.FlickrParameterKeys.Extras: Constants.FlickrParameterValues.MediumURL,
+                Constants.FlickrParameterKeys.Format: Constants.FlickrParameterValues.ResponseFormat,
+                Constants.FlickrParameterKeys.NoJSONCallback: Constants.FlickrParameterValues.DisableJSONCallback
+            ]
+            displayImageFromFlickrBySearch(methodParameters)
+        }
+        else {
+            setUIEnabled(true)
+            photoTitleLabel.text = "Lat should be [-90, 90].\nLon should be [-180, 180]."
+        }
+    }
+    
+    private func bboxString()->String{
+        guard let latitude = Double(latitudeTextField.text!), let longitude = Double(longitudeTextField.text!)
+            else {
+                print("Error encountered")
+                return "0,0,0,0"
+        }
+        let minimumLon = max(longitude - Constants.Flickr.SearchBBoxHalfWidth, Constants.Flickr.SearchLonRange.0)
+        let minimumLat = max(latitude - Constants.Flickr.SearchBBoxHalfHeight, Constants.Flickr.SearchLatRange.0)
+        
+        let maximumLon = min(longitude + Constants.Flickr.SearchBBoxHalfWidth, Constants.Flickr.SearchLonRange.1)
+        let maximumLat = min(latitude + Constants.Flickr.SearchBBoxHalfHeight, Constants.Flickr.SearchLatRange.1)
+        return "\(minimumLon),\(minimumLat),\(maximumLon),\(maximumLat)"
+    }
+    
+    // MARK: Flickr API
+    
+    private func displayImageFromFlickrBySearch(_ methodParameters: [String: Any]) {
+        let session = URLSession.shared
+        let request = URLRequest(url : flickrURLFromParameters(methodParameters))
+        let task = session.dataTask(with: request, completionHandler: {
+            (data, response, error) in
+            func displayError(error: String){
+                print(error)
+                performUIUpdatesOnMain {
+                    self.setUIEnabled(true)
+                    self.photoTitleLabel.text = "No photo returned"
+                    self.photoImageView.image = nil
+                }
+            }
+            
+            guard error == nil else{
+                print(error!.localizedDescription)
+                return
+            }
+            guard let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode >= 200 && statusCode <= 299 else{
+                displayError(error: "Request was unsuccessful")
+                return
+            }
+            
+            guard let data = data else{
+                displayError(error: "No Data")
+                return
+            }
+            
+            let parsedResult: AnyObject!
+            do {
+                parsedResult = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as AnyObject
+            }
+            catch{
+                return
+            }
+            
+            guard let photosDictionary = parsedResult[Constants.FlickrResponseKeys.Photos] as? [String: AnyObject] else{
+                displayError(error: "No photos key")
+                return
+            }
+            
+            guard let photoDictionary = photosDictionary[Constants.FlickrResponseKeys.Photo] as? [[String : AnyObject]] else{
+                displayError(error: "No photo key")
+                return
+            }
+            
+            let randomIndex = Int(arc4random_uniform(UInt32(photoDictionary.count)))
+            let photo = photoDictionary[randomIndex]
+            
+            guard let imageString = photo[Constants.FlickrResponseKeys.MediumURL] as? String, let imageTitle = photo[Constants.FlickrResponseKeys.Title] as? String else{
+                displayError(error: "Couldnt find image and title")
+                return
+            }
+            let imageUrl = URL(string: imageString)
+            do{
+                guard let imageData =  NSData(contentsOf: imageUrl!) else{
+                    return
+                }
+                performUIUpdatesOnMain {
+                    self.photoImageView.image = UIImage(data: imageData as Data)
+                    self.photoTitleLabel.text = imageTitle
+                    self.setUIEnabled(true)
+                }
+            }
+            
+            
+            
+            
+            
+        })
+        task.resume()
+    }
+    
+    // MARK: Helper for Creating a URL from Parameters
+    
+    private func flickrURLFromParameters(_ parameters: [String: Any]) -> URL {
+        
+        var components = URLComponents()
+        components.scheme = Constants.Flickr.APIScheme
+        components.host = Constants.Flickr.APIHost
+        components.path = Constants.Flickr.APIPath
+        components.queryItems = [URLQueryItem]()
+        
+        for (key, value) in parameters {
+            let queryItem = URLQueryItem(name: key, value: "\(value)")
+            components.queryItems!.append(queryItem)
+        }
+        
+        return components.url!
+    }
 }
+
+// MARK: - ViewController: UITextFieldDelegate
+
+extension ViewController: UITextFieldDelegate {
+    
+    // MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    // MARK: Show/Hide Keyboard
+    
+    @objc func keyboardWillShow(_ notification: Notification) {
+        if !keyboardOnScreen {
+            view.frame.origin.y -= keyboardHeight(notification)
+        }
+    }
+    
+    @objc func keyboardWillHide(_ notification: Notification) {
+        if keyboardOnScreen {
+            view.frame.origin.y += keyboardHeight(notification)
+        }
+    }
+    
+    @objc func keyboardDidShow(_ notification: Notification) {
+        keyboardOnScreen = true
+    }
+    
+    @objc func keyboardDidHide(_ notification: Notification) {
+        keyboardOnScreen = false
+    }
+    
+    func keyboardHeight(_ notification: Notification) -> CGFloat {
+        let userInfo = (notification as NSNotification).userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.cgRectValue.height
+    }
+    
+    func resignIfFirstResponder(_ textField: UITextField) {
+        if textField.isFirstResponder {
+            textField.resignFirstResponder()
+        }
+    }
+    
+    @IBAction func userDidTapView(_ sender: AnyObject) {
+        resignIfFirstResponder(phraseTextField)
+        resignIfFirstResponder(latitudeTextField)
+        resignIfFirstResponder(longitudeTextField)
+    }
+    
+    // MARK: TextField Validation
+    
+    func isTextFieldValid(_ textField: UITextField, forRange: (Double, Double)) -> Bool {
+        if let value = Double(textField.text!), !textField.text!.isEmpty {
+            return isValueInRange(value, min: forRange.0, max: forRange.1)
+        } else {
+            return false
+        }
+    }
+    
+    func isValueInRange(_ value: Double, min: Double, max: Double) -> Bool {
+        return !(value < min || value > max)
+    }
+}
+
+// MARK: - ViewController (Configure UI)
+
+private extension ViewController {
+    
+    func setUIEnabled(_ enabled: Bool) {
+        photoTitleLabel.isEnabled = enabled
+        phraseTextField.isEnabled = enabled
+        latitudeTextField.isEnabled = enabled
+        longitudeTextField.isEnabled = enabled
+        phraseSearchButton.isEnabled = enabled
+        latLonSearchButton.isEnabled = enabled
+        
+        // adjust search button alphas
+        if enabled {
+            phraseSearchButton.alpha = 1.0
+            latLonSearchButton.alpha = 1.0
+        } else {
+            phraseSearchButton.alpha = 0.5
+            latLonSearchButton.alpha = 0.5
+        }
+    }
+}
+
+// MARK: - ViewController (Notifications)
+
+private extension ViewController {
+    
+    func subscribeToNotification(_ notification: NSNotification.Name, selector: Selector) {
+        NotificationCenter.default.addObserver(self, selector: selector, name: notification, object: nil)
+    }
+    
+    func unsubscribeFromAllNotifications() {
+        NotificationCenter.default.removeObserver(self)
+    }
+}
+
 
